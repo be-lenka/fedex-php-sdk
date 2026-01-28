a23a873a2ddb4906810de6f0812ff0e261ac8f3a
tomas-378 <tomas.sentkeresty@belenka.com>
2026-01-28 13:39:39 +0100

    impl AuthorizationResponseDto

--- src/Dto/AuthorizationResponseDto.php
+++ src/Dto/AuthorizationResponseDto.php
@@ -4,5 +4,59 @@ namespace belenka\fedex\Authorization;
 
 final class AuthorizationResponseDto
 {
+    private string $accessToken;
 
+    private string $tokenType;
+
+    private int $expiresIn;
+
+    private string $scope;
+
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+
+    public function setAccessToken(string $accessToken): self
+    {
+        $this->accessToken = $accessToken;
+
+        return $this;
+    }
+
+    public function getTokenType(): string
+    {
+        return $this->tokenType;
+    }
+
+    public function setTokenType(string $tokenType): self
+    {
+        $this->tokenType = $tokenType;
+
+        return $this;
+    }
+
+    public function getExpiresIn(): int
+    {
+        return $this->expiresIn;
+    }
+
+    public function setExpiresIn(int $expiresIn): self
+    {
+        $this->expiresIn = $expiresIn;
+
+        return $this;
+    }
+
+    public function getScope(): string
+    {
+        return $this->scope;
+    }
+
+    public function setScope(string $scope): self
+    {
+        $this->scope = $scope;
+
+        return $this;
+    }
 }
